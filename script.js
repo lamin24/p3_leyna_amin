6f6afe8d35a453fe942af4031ef974c8e5c7beed
lamin24 <75296037+lamin24@users.noreply.github.com>
2021-03-01 21:33:20 -0800

    commented js out

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function showMenu() {
-  var x = document.getElementById("menu");
+// function showMenu() {
+//   var x = document.getElementById("menu");
 
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-//used https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// //used https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
